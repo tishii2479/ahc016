472025112a5eadedc6affd153b5ef3f1b54be8a7
Tatsuya Ishii <tishii2479@gmail.com>
2022-11-20 18:01:32 +0900

    final submit

--- src/bin/sol.rs
+++ src/bin/sol.rs
@@ -27,9 +27,7 @@ fn main() {
     time::start_clock();
 
     const QUERY_COUNT: usize = 100;
-    // FIXME: 最後に4.7に直す
     const CONSTRUCT_TIME_LIMIT: f64 = 4.7;
-    // const TIME_LIMIT: f64 = 4.8;
 
     let stdin = io::stdin();
     let stdout = io::stdout();
